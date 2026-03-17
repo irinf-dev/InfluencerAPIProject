2baae4449b88c8e7caa35fa4a9bdd328aeef765e
Murunwa Maphiri <murunwa.m@assendcreatives.co.za>
2026-03-17 14:09:07 +0200

    Nullable InfluencerId

--- BackendAPI/Models/Campaign.cs
+++ BackendAPI/Models/Campaign.cs
@@ -81,7 +81,7 @@ namespace BackendAPI.Models
         /// <summary>
         /// Influencer ID
         /// </summary>
-        public Guid InfluencerId { get; set; } //Look into allowing the InfluencerId to be nullable just in case a campaign doesn't have an influencer yet
+        public Guid? InfluencerId { get; set; } //Look into allowing the InfluencerId to be nullable just in case a campaign doesn't have an influencer yet
         public Influencer? Influencer { get; set; }
 
         //Look into adding a name field 
